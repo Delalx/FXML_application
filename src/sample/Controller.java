fec877cad8e1f63149c24970b31bb3572d2c1c21
Delalx <Delal-kesen@outlook.com>
2020-11-27 11:23:01 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,31 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
+import java.util.Date;
+
+public class Controller {
+
+    @FXML // fx:id="textArea1"
+    TextArea textArea; // Value injected by FXMLLoader
+
+
+    @FXML
+    void hej() {
+        textArea.appendText("Hej!\n");
+    }
+
+    @FXML
+    void nu() {
+        textArea.setText("Nu er det " + new Date().toString());
+    }
+
+    @FXML
+    void terning() {
+        int terningeKast = (int)(Math.random() * 6) + 1;
+        textArea.setText("Terningen viser " + terningeKast + "\n");
+    }
+
+}
